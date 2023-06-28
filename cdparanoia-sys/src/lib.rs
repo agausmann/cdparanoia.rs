84463df5d4d4068064291116b58fc8cfdc5abc33
Adam Gausmann <adam@gaussian.dev>
2023-06-28 00:19:48 -0500

    Add sanity test

--- cdparanoia-sys/src/lib.rs
+++ cdparanoia-sys/src/lib.rs
@@ -3,3 +3,24 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::CStr;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let ffi_cdda_version = unsafe { CStr::from_ptr(cdda_version()) };
+        let ffi_paranoia_version = unsafe { CStr::from_ptr(paranoia_version()) };
+
+        let cdda_version = ffi_cdda_version.to_str().expect("non-UTF8 version string");
+        let paranoia_version = ffi_paranoia_version
+            .to_str()
+            .expect("non-UTF8 version string");
+
+        eprintln!("cdda version: {}", cdda_version);
+        eprintln!("paranoia version: {}", paranoia_version);
+    }
+}
