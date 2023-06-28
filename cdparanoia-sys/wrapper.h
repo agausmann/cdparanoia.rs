d905ce694cc5156372bf97ae5d1bcd512bfbcf9f
Adam Gausmann <adam@gaussian.dev>
2023-06-28 00:06:15 -0500

    Add wrapper for IS_AUDIO macro

--- cdparanoia-sys/wrapper.h
+++ cdparanoia-sys/wrapper.h
@@ -1,2 +1,10 @@
 #include <cdda_interface.h>
 #include <cdda_paranoia.h>
+
+#include <stdbool.h>
+
+/* Wrapper for IS_AUDIO macro */
+bool is_audio(const cdrom_drive *d, int i)
+{
+    return IS_AUDIO(d, i);
+}
